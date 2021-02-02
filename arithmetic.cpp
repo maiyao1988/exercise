20eb436cd003ed3e87cafdf4e93fbce90652f95d
maiyao <maiyao@tencent.com>
2021-02-02 22:46:03 +0800

    浮点数加法

--- arithmetic.cpp
+++ arithmetic.cpp
@@ -1,27 +1,31 @@
 #include <stdio.h>
 
-unsigned half_adder(unsigned m1, unsigned m2, unsigned &out_carry) {
+unsigned half_adder(unsigned m1, unsigned m2, unsigned &out_carry)
+{
     unsigned r = m1 ^ m2;
     out_carry = m1 & m2;
     return r;
 }
 
-unsigned adder(unsigned m1, unsigned m2, unsigned int carry, unsigned &out_carry) {
+unsigned adder(unsigned m1, unsigned m2, unsigned int carry, unsigned &out_carry)
+{
 
     unsigned x = 0;
     unsigned y = half_adder(m1, m2, x);
 
     unsigned carry2 = 0;
     unsigned w = half_adder(y, carry, carry2);
-    
+
     out_carry = carry2 | x;
     return w;
 }
 
-unsigned int add(unsigned a, unsigned b, unsigned &carry_left) {
+unsigned int add(unsigned a, unsigned b, unsigned &carry_left)
+{
 
     unsigned r = 0;
-    for (int i = 0; i < 32; i++) {
+    for (int i = 0; i < 32; i++)
+    {
         unsigned int mask = 1 << i;
         unsigned int m1 = (a & mask) >> i;
         unsigned int m2 = (b & mask) >> i;
@@ -35,33 +39,38 @@ unsigned int add(unsigned a, unsigned b, unsigned &carry_left) {
     return r;
 }
 
-
-
-int signed_add(int a, int b, unsigned &of) {
+int signed_add(int a, int b, unsigned &of)
+{
     unsigned cf = 0;
     int out = add(a, b, cf);
     //符号位相同才存在溢出,
-    if ((a & (1<<31)) == (b & (1<<31))) {
+    if ((a & (1 << 31)) == (b & (1 << 31)))
+    {
         //如果结果符号与输入相同,则不溢出,否则溢出
-        of = (a>>31 & 1) != (out>>31 & 1);
+        of = (a >> 31 & 1) != (out >> 31 & 1);
     }
-    else {
+    else
+    {
         //不同的时候不可能溢出
         of = 0;
     }
     return out;
 }
 
-int neg(int a) {
-    return (~a)+1;
+int neg(int a)
+{
+    return (~a) + 1;
 }
-int signed_div(int a, int b, int &rem) {
+int signed_div(int a, int b, int &rem)
+{
     int dividend = a;
     unsigned long long divider = b;
-    if (a < 0) {
+    if (a < 0)
+    {
         dividend = neg(a);
     }
-    if (b < 0) {
+    if (b < 0)
+    {
         divider = neg(b);
     }
     divider = divider << 32;
@@ -72,73 +81,189 @@ int signed_div(int a, int b, int &rem) {
     //1.将除数扩展到64bit,并将除数移到高32bit
     //2.不断右移除数,如果除数比被除数大则这位商为0,否则为1,商为1时,将被除数-除数, 商不断左移
     //3.循环32次
-    for (int i = 0; i < 32; i++) {
+    for (int i = 0; i < 32; i++)
+    {
         unsigned c = 0;
         divider = divider >> 1;
-        if (divider > dividend) {
+        if (divider > dividend)
+        {
             c = 0;
         }
-        else {
+        else
+        {
             unsigned of = 0;
             //dividend - divider
-            dividend = signed_add(dividend , neg(divider), of);
+            dividend = signed_add(dividend, neg(divider), of);
             c = 1;
         }
         r = (r << 1) | c;
     }
     //最后被除数就是余数
     rem = (int)dividend;
-    if (((a >> 31) & 1) != ((b >> 31) & 1)) {
+    if (((a >> 31) & 1) != ((b >> 31) & 1))
+    {
         //输入异号结果即为负
         r = neg(r);
         rem = neg(rem);
     }
-    
+
     return r;
 }
 
-unsigned int mul(unsigned a, unsigned b, unsigned &carry_left) {
+unsigned int mul(unsigned a, unsigned b, unsigned &carry_left)
+{
     unsigned sum = 0;
     carry_left = 0;
-    while(b!=0) {
+    while (b != 0)
+    {
         unsigned t = b & 1;
-        if (t) {
+        if (t)
+        {
             unsigned cf = 0;
             sum = add(sum, a, cf);
-            if (cf!=0) {
+            if (cf != 0)
+            {
                 carry_left = 1;
             }
         }
-        a = a<<1;
-        b = b>>1;
+        a = a << 1;
+        b = b >> 1;
     }
     return sum;
 }
 
-
-int signed_mul(int a, int b, unsigned &of) {
+int signed_mul(int a, int b, unsigned &of)
+{
     unsigned cf = 0;
     int out = mul(a, b, cf);
     //不同号,结果肯定是负数,否则溢出
-    if ((a & (1<<31)) != (b & (1<<31))) {
-        of = (out>>1 & 1) == 0;
+    if ((a & (1 << 31)) != (b & (1 << 31)))
+    {
+        of = (out >> 1 & 1) == 0;
     }
-    else if ((a & (1<<31)) == (b & (1<<31))) {
+    else if ((a & (1 << 31)) == (b & (1 << 31)))
+    {
         //同号,结果位正,如果符号位位1, 则溢出,
-        of = out>>31 & 1;
+        of = out >> 31 & 1;
     }
     return out;
 }
 
-int addINT(int a, int b, unsigned &of) {
+int addINT(int a, int b, unsigned &of)
+{
     int r = signed_add(a, b, of);
-    printf("addInt a=%d b=%d r=%d of=%u\n", a, b, r, of!=0);
+    printf("addInt a=%d b=%d r=%d of=%u\n", a, b, r, of != 0);
     return r;
 }
 
-int mulINT(int a, int b, unsigned &of) {
+int mulINT(int a, int b, unsigned &of)
+{
     int r = signed_mul(a, b, of);
-    printf("mulINT a=%d b=%d r=%d of=%u\n", a, b, r, of!=0);
+    printf("mulINT a=%d b=%d r=%d of=%u\n", a, b, r, of != 0);
+    return r;
+}
+
+void dump_float(float a) {
+    unsigned tail = (unsigned&)a << 9 >> 9;
+    unsigned level = (unsigned&)a << 1 >> 1 >> 23;
+    unsigned sign = (unsigned&)a>>31;
+    printf("sign:%x level:%x tail:%x\n", sign, level, tail);
+}
+
+float float_add(float a, float b) {
+    //dump_float(a);
+    //dump_float(b);
+
+    unsigned tail = (unsigned&)a << 9 >> 9;
+    unsigned level = (unsigned&)a << 1 >> 1 >> 23;
+    unsigned sign = (unsigned&)a>>31;
+
+    unsigned tail2 = (unsigned&)b << 9 >> 9;
+    unsigned level2 = (unsigned&)b << 1 >> 1 >> 23;
+    unsigned sign2 = (unsigned&)b>>31;
+
+    
+    unsigned bigTail = 0;
+    unsigned bigLevel = 0;
+    unsigned bigSign = 0;
+
+    unsigned smallTail = 0;
+    unsigned smallLevel = 0;
+    unsigned smallSign = 0;
+    if (level > level2) {
+        bigTail = tail;
+        bigLevel = level;
+        bigSign = sign;
+
+        smallTail = tail2;
+        smallLevel = level2;
+        smallSign = sign2;
+    }
+    else {
+        bigTail = tail2;
+        bigLevel = level2;
+        bigSign = sign2;
+
+        smallTail = tail;
+        smallLevel = level;
+        smallSign = sign;
+    }
+
+    unsigned levelMargin = bigLevel - smallLevel;
+
+    smallTail = (1 << 23) | smallTail;
+    bigTail = (1 << 23) | bigTail;
+
+    //对阶
+    smallTail = smallTail >> levelMargin;
+
+    if (bigSign == 1) {
+        bigTail = neg(bigTail);
+    }
+    if (smallSign == 1) {
+        smallTail = neg(smallTail);
+    }
+
+
+    //尾数相加
+    unsigned st = smallTail + bigTail;
+    //printf("%x\n", st);
+    unsigned sgr = 0;
+    if ((st >> 31) & 1) {
+        sgr = 1;
+        //负数取反,由符号为决定正负
+        st = neg(st);
+    }
+    if (st == 0) {
+        //对阶后加法,结果直接为0,结果就是0,0是特殊的表达
+        return 0;
+    }
+
+    //printf("%x\n", st);
+    
+    
+    //标准化,保证24位总是1,而且是第一个1
+    if ((st & (1 << 23)) == 0){
+        if(st > (1<<23)) {
+            while ((st & (1<<23)) == 0) {
+                st = st >> 1;
+                bigLevel += 1;
+            }
+        }
+        else {
+            while ((st & (1<<23)) == 0) {
+                st = st << 1;
+                bigLevel -= 1;
+            }
+        }
+    }
+    //printf("%x\n", st);
+
+    st = st << 9 >> 9;
+
+    unsigned u = (sgr << 31 | bigLevel << 23 | st);
+    float r = (float&)u;
+
     return r;
 }
 
@@ -159,8 +284,36 @@ unsigned int mul_para(unsigned a, unsigned b, unsigned n) {
 }
 */
 
+int main()
+{
+    
+    float r = float_add(1.1, 0.5);
+    //dump_float(r);
+    printf("%.7f\n", r);
+    
 
-int main() {
+    r = float_add(0, -0.5);
+    //dump_float(r);
+    printf("%.7f\n", r);
+    
+
+    r = float_add(-1.1, -0.5);
+    //dump_float(r);
+    printf("%.7f\n", r);
+
+    r = float_add(900, -0.5);
+    //dump_float(r);
+    printf("%.7f\n", r);
+
+
+    r = float_add(0.3, -0.44);
+    //dump_float(r);
+    printf("%.7f\n", r);
+
+    /*
+    float f = 1.1 + 0.5;
+    printf("%.7f\n", f);
+    */
     /*
     unsigned sum = 0;
     addINT(2, 3, sum);
@@ -181,36 +334,27 @@ int main() {
     mulINT(-213, -155, mul);
     mulINT(-2147483648, -1, mul);
     */
-    int rem = 0;
-    int r = signed_div(-16, 5, rem);
-    printf("%d %d\n", r, rem);
     /*
-    unsigned int a = -2147483648;
-    unsigned int b = 0;
-    unsigned of = 0;
-    int r = signed_add(a, b, of);
-
-    if (of != 0) {
-        printf("overflow!!!\n");
-    }
-    printf("%d\n", r);
+    int rem = 0;
+    int r = signed_div(7, 2, rem);
+    printf("%d %d\n", r, rem);
+    r = signed_div(307, 15, rem);
+    printf("%d %d\n", r, rem);
+    r = signed_div(2147483647, 35, rem);
+    printf("%d %d\n", r, rem);
+    r = signed_div(-7, 2, rem);
+    printf("%d %d\n", r, rem);
+    r = signed_div(-307, 15, rem);
+    printf("%d %d\n", r, rem);
+    r = signed_div(-2147483647, 35, rem);
+    printf("%d %d\n", r, rem);
+    r = signed_div(-7, -2, rem);
+    printf("%d %d\n", r, rem);
+    r = signed_div(-307, -15, rem);
+    printf("%d %d\n", r, rem);
+    r = signed_div(-2147483647, -35, rem);
+    printf("%d %d\n", r, rem);
     */
+
     return 0;
-    
-   
-  /*
-    unsigned a = 7;
-    unsigned b = 8;
-    unsigned r[32] = {0};
-    unsigned sum = 0;
-    unsigned carry_left = 0;
-    for(int i=0; i < 32; i++) {
-        //这里开n个线程或者协程即可并行
-        r[i] = mul_para(a, b, i);
-    }
-    for (int i = 0; i < 32; i++) {
-        sum = add(sum, r[i], carry_left);
-    }
-    printf("sum %d\n", sum);
-  */
 }
